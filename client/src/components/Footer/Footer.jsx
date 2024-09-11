2a4adb72500c520d28950b4486bc2a65cbe20a56
Борислав Христов <hristovborislav45@gmail.com>
2024-09-11 18:30:21 +0300

    Work on animation

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,16 +4,15 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="home-footer">
-      <h2 className='logo-text'>Footer here</h2>
-      <img src="../../assets/pc.png" alt="Computer Left" className="computer left" />
+      <img src="/src/assets/pc.png" alt="Computer Left" className="computer left" />
       <motion.img
-        src="../../assets/pc.png"
+        src="/src/assets/mail.png"
         alt="Mail"
         className="mail"
-        animate={{ x: ["0%", "50%", "100%"] }}
-        transition={{ duration: 4, repeat: Infinity, repeatType: "loop" }}
+        animate={{ x: ["50%", "1500%", "2800%"] }}
+        transition={{ duration: 1, repeat: Infinity, repeatType: "loop" }}
       />
-      <img src="../../assets/pc.png" alt="Computer Right" className="computer right" />
+      <img src="/src/assets/pc.png" alt="Computer Right" className="computer right" />
     </footer>
   );
 }
