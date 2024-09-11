b9dbd0a0c1a5232f3d932a3e83d86a514aa84233
Борислав Христов <hristovborislav45@gmail.com>
2024-09-11 11:44:22 +0300

    Update Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,19 @@
+import { motion } from 'framer-motion';
+import './Footer.css';
+
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="home-footer">
+      <img src="../../assets/user.jpg" alt="Computer Left" className="computer left" />
+      <motion.img
+        src="../../assets/user.jpg"
+        alt="Mail"
+        className="mail"
+        animate={{ x: ["0%", "50%", "100%"] }}
+        transition={{ duration: 4, repeat: Infinity, repeatType: "loop" }}
+      />
+      <img src="../../assets/user.jpg" alt="Computer Right" className="computer right" />
+    </footer>
   );
 }
 
