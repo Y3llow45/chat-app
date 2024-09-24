abb6535403a78e269c6b53bfb486a55fd7adddb7
Борислав Христов <hristovborislav45@gmail.com>
2024-09-24 17:30:56 +0300

    Create socket.js

---
+++ server/socket.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (msg) => {
+    console.log('Message received:', msg);
+    io.emit('message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
