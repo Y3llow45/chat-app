bfcfed6307b5f2cd0d130ac044f9e2ff3f1fbb09
Борислав Христов <hristovborislav45@gmail.com>
2024-09-25 15:53:05 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,95 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require('express')
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const generateToken = require('./services/genToken');
+const bodyParser = require('body-parser');
+const User = require("./models/User");
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const app = express()
+const PORT = parseInt(process.env.PORT, 10)
+const saltRounds = parseInt(process.env.saltRounds, 10)
+const AtlasUri = process.env.ATLASURI;
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+app.use(cors());
+app.use(bodyParser.json());
 
-  socket.on('message', (msg) => {
-    console.log('Message received:', msg);
-    io.emit('message', msg);
-  });
+mongoose.connect(AtlasUri).then(() => {
+  console.log('Connected to db');
+})
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+app.post('/register', async (req, res) => {
+  try {
+    let { username, password } = req.body;
+    const testUsername = await User.find({ username: username })
+    console.log('find')
+    if (testUsername.length > 0) {
+      console.log(testUsername)
+      return res.status(400).json({ message: 'Username already exists' });
+    }
+    console.log('continnuing')
+    bcrypt
+      .hash(password, saltRounds)
+      .then(hash => {
+        let newUser = new User({ username: username, password: hash })
+        console.log('before saving')
+        newUser.save();
+        console.log('saved')
+      })
+      .catch((err) => { throw err })
+  }
+  catch (error) {
+    res.statusMessage = `${error}`;
+    return res.status(500).send();
+  }
+  console.log('acc created')
+  res.status(201).json({ message: 'Account created' });
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.find({ username: username });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+    bcrypt
+      .hash(user.password, saltRounds)
+      .then(hash => {
+        user.password = hash;
+      })
+      .catch((err) => { throw err })
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      res.status(401).json({ error: 'Invalid credentials' });
+      return;
+    }
+    const token = generateToken(user._id, user.username);
+    res.status(200).json({ message: 'Sign in successful', token, username: user.username });
+
+  } catch (error) {
+    console.error('Error during sign-in:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
+app.get('/checkUsername/:username', async (req, res) => {
+  try {
+    let { username } = req.params;
+    user = await User.findOne({ username: input });
+    if (!lesson) {
+      return res.status(200).json({ message: 'false' });
+    } else {
+      return res.status(200).json({ message: 'true' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`)
+})
