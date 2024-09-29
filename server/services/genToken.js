99268820aeacc45b18b266b6dd48f0f17503ce81
Борислав Христов <hristovborislav45@gmail.com>
2024-09-29 19:50:36 +0300

    Create genToken.js

---
+++ server/services/genToken.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const SSKEY = process.env.SSKEY;
+
+const generateToken = (userId, username, role) => {
+  userId = userId.toString();
+  const token = jwt.sign({ userId, username: username, role: role }, SSKEY);//, { expiresIn: '1d' }
+  return token;
+};
+
+module.exports = generateToken;
