db1da666dae1139a2532d2bfc50ced2643d921e9
Борислав Христов <hristovborislav45@gmail.com>
2024-10-03 18:50:27 +0300

    Add context providers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,34 +7,40 @@ import SignUp from './components/SignUp/SignUp';
 import SignIn from './components/SignIn/SignIn';
 import Chats from './components/Chats/Chats';
 import Footer from './components/Footer/Footer';
+import { UsernameProvider } from './contexts/UsernameContext';
+import { RoleProvider } from './contexts/RoleContext';
 
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <div className="App">
-      <ToastContainer
-        className="add-toast-container"
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={true}
-        closeOnClick={true}
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable={false}
-        pauseOnHover={false}
-        theme="light"
-      />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chats" element={<Chats />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
-      <Footer />
-    </div>
+    <UsernameProvider>
+      <RoleProvider>
+        <div className="App">
+          <ToastContainer
+            className="add-toast-container"
+            position="top-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick={true}
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable={false}
+            pauseOnHover={false}
+            theme="light"
+          />
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chats" element={<Chats />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+          <Footer />
+        </div>
+      </RoleProvider>
+    </UsernameProvider>
   )
 }
 
