3302688a635e272edb7cec26fbaff00f6aebb1f5
Борислав Христов <hristovborislav45@gmail.com>
2024-10-07 20:32:21 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose")
+import genRandomPfp from '../services/genRandomPfp'
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-  role: String,
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  profilePic: { type: String, default: () => genRandomPfp() }
 })
 
 module.exports = mongoose.model("User", userSchema)
