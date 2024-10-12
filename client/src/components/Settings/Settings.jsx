d05e454512534abaa5efb8a5cbd5d13cfb3ff3f3
Борислав Христов <hristovborislav45@gmail.com>
2024-10-12 21:26:17 +0300

    Update Settings.jsx

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,6 +1,59 @@
-const [selectedPfp, setSelectedPfp] = useState(currentUserPfp);
-const handlePfpChange = (newPfp) => setSelectedPfp(newPfp);
+import React, { Component } from 'react';
+import { withUsernameAuth } from '../../contexts/UsernameContext';
+import { withPfpAuth } from '../../contexts/PfpContext';
+import { save } from '../../services/Services';
+import navWrap from '../Form/NavWrap/NavWrap';
+import './Settings.css';
 
-const handleSave = async () => {
-  await updatePfp(selectedPfp);
-};
+const availablePfps = [1, 2, 3, 4];
+
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedPfp: this.props.userPfp || availablePfps[0],
+    };
+  }
+
+  handlePfpSelect = (pfp) => {
+    this.setState({ selectedPfp: pfp });
+  };
+
+  handleSave = () => {
+    const { setUserPfp } = this.props;
+    const { selectedPfp } = this.state;
+    setUserPfp(selectedPfp);
+    save(selectedPfp);
+  };
+
+  render() {
+    const { username } = this.props;
+    const { selectedPfp } = this.state;
+
+    return (
+      <div className="settings-container">
+        <h2>Welcome, {username}!</h2>
+
+        <h3>Select your profile picture</h3>
+        <div className="pfp-selection">
+          {availablePfps.map((pfp) => (
+            <img
+              key={pfp}
+              src={`pic${pfp}.jpg`}
+              alt={`Profile ${pfp}`}
+              className={`pfp-item ${selectedPfp === pfp ? 'selected' : ''}`}
+              onClick={() => this.handlePfpSelect(pfp)}
+            />
+          ))}
+        </div>
+
+        <button className="save-button" onClick={this.handleSave}>
+          Save
+        </button>
+      </div>
+    );
+  }
+}
+
+export default navWrap(withUsernameAuth(withPfpAuth(Settings)));
