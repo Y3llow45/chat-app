06b94f0bfb275341c76f15032bb4aa587bb3bc01
Борислав Христов <hristovborislav45@gmail.com>
2024-10-18 19:05:44 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,9 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true },
-  profilePic: { type: Number, default: 0 }
+  profilePic: { type: Number, default: 0 },
+  friends: [{ type: String }],
+  pendingRequests: [{ type: String }]
 })
 
 module.exports = mongoose.model("User", userSchema)
