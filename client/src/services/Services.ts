ca08c669987547c32e88f4a9801069fae1590e85
Борислав Христов <hristovborislav45@gmail.com>
2024-10-19 20:16:09 +0300

    Update Services.ts

--- client/src/services/Services.ts
+++ client/src/services/Services.ts
@@ -1,74 +1,47 @@
 import { displayInfo } from '../components/Notify/Notify';
 const url = 'http://localhost:5242/';
 
-export const updatePfp = (index: number) => {
+const fetchWithAuth = (endpoint: string, options: RequestInit = {}) => {
   const token = localStorage.getItem('token');
   if (!token) return;
-  return fetch(`${url}updatePfp/${index}`, {
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': token,
+    ...(options.headers || {}),
+  };
+
+  return fetch(`${url}${endpoint}`, { ...options, headers })
+    .then(res => res.json())
+    .catch(error => console.log(error));
+};
+
+export const updatePfp = (index: number) => {
+  return fetchWithAuth(`updatePfp/${index}`, {
     method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': token
-    }
-  })
+  });
 };
 
 export const getRole = () => {
-  const token = localStorage.getItem('token');
-  if (!token) return;
-  return fetch(`${url}api/getUserRole`, { headers: { 'Authorization': token } })
-    .then(res => res.json())
-    .then((data) => {
-      return data
-    })
-    .catch(
-      (error) => console.log(error)
-    );
-}
+  return fetchWithAuth('api/getUserRole');
+};
 
 export const searchUsers = (username: string) => {
-  const token = localStorage.getItem('token');
-  if (!token) return;
-  return fetch(`${url}searchUsers/${username}`, { headers: { 'Authorization': token } })
-    .then(res => res.json())
-    .then((data) => {
-      return data
-    })
-    .catch(
-      (error) => console.log(error)
-    );
-}
+  return fetchWithAuth(`searchUsers/${username}`);
+};
 
 export const sendFriendRequest = (friendUsername: string) => {
-  const token = localStorage.getItem('token');
-  if (!token) return;
-
-  return fetch(`${url}/friendRequest`, {
+  return fetchWithAuth('friendRequest', {
     method: 'POST',
-    headers: {
-      'Authorization': token,
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify({ friendUsername }),
-  })
-    .then(res => res.json())
-    .catch(error => console.log(error));
+  });
 };
 
 export const acceptFriendRequest = (requesterUsername: string) => {
-  const token = localStorage.getItem('token');
-  if (!token) return;
-
-  return fetch(`${url}/acceptFriendRequest`, {
+  return fetchWithAuth('acceptFriendRequest', {
     method: 'POST',
-    headers: {
-      'Authorization': token,
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify({ requesterUsername }),
-  })
-    .then(res => res.json())
-    .catch(error => console.log(error));
+  });
 };
 
 export const signUp = (username: string, email: string, password: string) => {
